e9bcd720d6745a2120e1d8f2566b6e47552450c0
angpao <angpao.kittimasak@gmail.com>
2019-04-11 10:55:56 +0700

    Add RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddPageComponent } from './components/add-page/add-page.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "add", component: AddPageComponent },
+  { path: "edit", component: EditPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
+import { AddPageComponent } from './components/add-page/add-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditPageComponent,
+    AddPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
